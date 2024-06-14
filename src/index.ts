243d8224bb2b777f5e0f318ce62a108c02a80cba
moganesan <developer.moganesan@gmail.com>
2024-06-14 22:33:57 +0530

    removed moralis api

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { Signer, ethers } from "ethers";
 import dotenv from "dotenv";
 import { getDatabase, ref, set } from "firebase/database";
 import { erc20Abi } from "viem";
-import Moralis from "moralis";
 import uniswapAbi from "./uniswapAbi.json";
 import { app } from "./firebase";
 
@@ -35,16 +34,7 @@ const uniswapRouter = "0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24";
 
 const routerContract = new ethers.Contract(uniswapRouter, uniswapAbi, signer);
 
-// Initialize moralis
-async function InitializeMoralis() {
-  await Moralis.start({
-    apiKey:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjcyYmUwY2ZhLWIzNTgtNDRkMS04MDU3LTAwYWUxMzRiMmE5OCIsIm9yZ0lkIjoiMTQ2NDciLCJ1c2VySWQiOiIxMDk1MyIsInR5cGVJZCI6ImQ5YmJmMjg1LTM0MGUtNGYzYy04ZTUwLWU1NGRmZWY2MTc5NCIsInR5cGUiOiJQUk9KRUNUIiwiaWF0IjoxNzE4Mjg1MDQwLCJleHAiOjQ4NzQwNDUwNDB9.LiBt5qdtrChVjaNVLbkwxwrsFZ6ceJLm5QV1IrTeoDU",
-  });
-}
-
 async function main() {
-  await InitializeMoralis();
   const amountInUSDC = 1000;
   await convertERC20ToUSDCTAndTransferToPlatformAddress(amountInUSDC);
 }
@@ -138,21 +128,6 @@ async function getAmountsOutMin(amountIn: string) {
     ethers.utils.parseUnits(amountIn, 18),
     path
   );
-}
-
-async function USDTToERC20(token: string, amountInUSDC: number) {
-  const tokenPrice = await Moralis.EvmApi.token
-    .getTokenPrice({
-      chain: "0xa4b1",
-      include: "percent_change",
-      address: token,
-    })
-    .then((res) => res.raw);
-  console.log(tokenPrice);
-
-  const tokenToUscAmount = amountInUSDC / tokenPrice.usdPrice;
-  console.log("Token To USDC", tokenToUscAmount);
-  return tokenToUscAmount;
 }
 
 // check token allowance
